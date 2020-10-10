bc8ea13bf5a207cc6dc7ba6d2d01caf6daae7101
afif ahmed <afifahmed456123@gmail.com>
2020-10-10 17:09:52 +0530

    updates

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -130,7 +130,7 @@ const HamburgerMenu = ({
                                         closeMenu()
                                         setTimeout(() => {
                                             scrollTo(
-                                              window.innerHeight * 5.5 +
+                                              window.innerHeight * 5.2 +
                                               96
                                             ) //put in the value you wanna scroll to
                                         }, 750)
