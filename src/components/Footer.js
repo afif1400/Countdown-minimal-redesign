93ff145d1495b7b2eb348c035fa73a2277b404d6
clearly-outsane <1starwarrior516@gmail.com>
2020-10-14 21:36:20 +0530

    credits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,61 +3,80 @@ import InstagramIcon from '@material-ui/icons/Instagram'
 import MailOutlineIcon from '@material-ui/icons/MailOutline'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import FacebookIcon from '@material-ui/icons/Facebook'
+import Typography from '@material-ui/core/Typography'
 
 const Footer = () => {
     const fontSize = 24
     return (
-        <div className="footer">
+        <div className='footer'>
             <div style={{ backgroundColor: 'black' }}>
                 <div
                     style={{
                         display: 'flex',
-                        justifyContent: 'center',
+                        justifyContent: 'space-between',
                         alignItems: 'center',
-                        paddingTop: 24,
-                        paddingBottom: 24,
+                        padding: 24,
                     }}
                 >
-                    <a href='mailto:contact@tedxpesu.com'>
+                    <div>
+                        <Typography
+                            variant='subtitle2'
+                            style={{ color: 'white' }}
+                        >
+                            Designed by Vinay
+                        </Typography>
+                    </div>
+                    <div>
                         {' '}
-                        <MailOutlineIcon
-                            style={{
-                                fontSize: fontSize,
-                                color: 'white',
-                                margin: '24px',
-                            }}
-                        />
-                    </a>
-                    <a href='https://linkedin.com/company/tedxpesu'>
+                        <a href='mailto:contact@tedxpesu.com'>
+                            <MailOutlineIcon
+                                style={{
+                                    fontSize: fontSize,
+                                    color: 'white',
+                                    margin: '24px',
+                                }}
+                            />
+                        </a>
+                        <a href='https://linkedin.com/company/tedxpesu'>
+                            {' '}
+                            <LinkedInIcon
+                                style={{
+                                    fontSize: fontSize,
+                                    color: 'white',
+                                    margin: '24px',
+                                }}
+                            />
+                        </a>
+                        <a href='https://instagram.com/tedxpesu'>
+                            {' '}
+                            <InstagramIcon
+                                style={{
+                                    fontSize: fontSize,
+                                    color: 'white',
+                                    margin: '24px',
+                                }}
+                            />
+                        </a>
+                        <a href='https://facebook.com/tedxpesu'>
+                            {' '}
+                            <FacebookIcon
+                                style={{
+                                    fontSize: fontSize,
+                                    color: 'white',
+                                    margin: '24px',
+                                }}
+                            />
+                        </a>
+                    </div>
+                    <div>
                         {' '}
-                        <LinkedInIcon
-                            style={{
-                                fontSize: fontSize,
-                                color: 'white',
-                                margin: '24px',
-                            }}
-                        />
-                    </a>
-                    <a href='https://instagram.com/tedxpesu'>
-                        {' '}
-                        <InstagramIcon
-                            style={{
-                                fontSize: fontSize,
-                                color: 'white',
-                                margin: '24px',
-                            }}
-                        />
-                    </a>
-                    <a href='https://facebook.com/tedxpesu'>
-                        {' '}
-                        <FacebookIcon
-                            style={{
-                                fontSize: fontSize,
-                                color: 'white',
-                                margin: '24px',
-                            }}
-                        />
-                    </a>
+                        <Typography
+                            variant='subtitle2'
+                            style={{ color: 'white' }}
+                        >
+                            Co-developed by Afif
+                        </Typography>
+                    </div>
                 </div>
             </div>
         </div>
